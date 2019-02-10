509ce8898f9cb3f5ac53ae3df8af9ce4a56b34b3
Abrar Tajwar <abrartajwar619@gmail.com>
2019-02-10 17:38:56 +0000

    Update BlackJack.java

--- BlackJack/src/blackjack/BlackJack.java
+++ BlackJack/src/blackjack/BlackJack.java
@@ -8,30 +8,30 @@ package blackjack;
 import java.util.Scanner;
 
 /**
- *
+ * need to add exception need to handle closing algorithm need to solve the problem with design patterns
  * @author Abrar Tajwar
  */
 public class BlackJack {
 
    public static void main(String[] args){
-		
+
 		System.out.println("Welcome to Blackjack!");
-		
+
 		//playingDeck will be the deck the dealer holds
 		Deck playingDeck = new Deck();
 		playingDeck.createFullDeck();
 		playingDeck.shuffleDeck();
-		
+
 		//playerCards will be the cards the player has in their hand
 		Deck playerCards = new Deck();
 		//playerMoney holds players cash - we will be lazy and use doubles instead of bigdecimals
 		double playerMoney = 100.0;
 		//dealerCards will be the cards the dealer has in their hand
 		Deck dealerCards = new Deck();
-		
+
 		//Scanner for user input
 		Scanner userInput = new Scanner(System.in);
-		
+
 		//Play the game while the player has money
 		//Game loop
 while(playerMoney>0){
@@ -44,31 +44,31 @@ while(playerMoney>0){
 		System.out.println("You cannot bet more than you have.");
 		break;
 	}
-	
+
 	System.out.println("Dealing...");
 	//Player gets two cards
 	playerCards.draw(playingDeck);
 	playerCards.draw(playingDeck);
-	
+
 	//Dealer gets two cards
 	dealerCards.draw(playingDeck);
 	dealerCards.draw(playingDeck);
-			
+
 			//While loop for drawing new cards
 			while(true)
 			{
 				//Display player cards
 				System.out.println("Your Hand:" + playerCards.toString());
-				
+
 				//Display Value
 				System.out.println("Your hand is currently valued at: " + playerCards.cardsNumber());
-				
+
 				//Display dealer cards
 				System.out.println("Dealer Hand: " + dealerCards.cardGetter(0).toString() + " and [hidden]");
-				
+
 				//What do they want to do
 				System.out.println("Would you like to (1)Hit or (2)Stand");
-				int response = userInput.nextInt();	
+				int response = userInput.nextInt();
 				//They hit
 				if(response == 1){
 					playerCards.draw(playingDeck);
@@ -81,14 +81,14 @@ while(playerMoney>0){
 						break;
 					}
 				}
-				
+
 				//Stand
 				if(response == 2){
 					break;
 				}
-				
+
 			}
-				
+
 			//Reveal Dealer Cards
 			System.out.println("Dealer Cards:" + dealerCards.toString());
 			//See if dealer has more points than player
@@ -131,14 +131,14 @@ while(playerMoney>0){
 			playerCards.moveAllToDeck(playingDeck);
 			dealerCards.moveAllToDeck(playingDeck);
 			System.out.println("End of Hand.");
-			
+
 		}
 		//Game is over
 		System.out.println("Game over! You lost all your money. :(");
-		
+
 		//Close Scanner
 		userInput.close();
-		
+
 	}
-    
+
 }
